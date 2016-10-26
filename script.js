d61a587c04698db71b2fc3bfbe58b034a742b9f1
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-10-26 11:10:57 +0200

    first

---
+++ script.js
@@ -0,0 +1,31 @@
+// Using jQuery
+
+$(document).ready(function() {
+
+  var count = 6;
+
+
+  $('.bonbon').draggable({
+    containment : 'main',    revert : 'invalid'
+  });
+
+  $('.dropzone').droppable({
+    drop : function() {
+      if(count >0){
+        count--;
+        var newmsg = "Plus que " + count + " bonbons !";
+        $('#compteur').text(newmsg);
+      }
+      else {
+        $('#message,#compteur').remove();
+        $('main').append('Bien joué !</br><a href="index.html">Rejouer</a>');
+        alert("bien joué");
+      }
+
+
+    }
+  });
+
+
+
+});
